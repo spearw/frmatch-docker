4638d7b85ee9fab2b4aafe4d49ead2898a55c843
spearw <spearw@gmail.com>
2024-09-16 15:27:52 -0400

    Add lfs pull

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+git lfs pull
 docker run -it -w /root --platform linux/amd64 --mount type=bind,source="$(pwd)"/tests,target=/root/tests spearw/frmatch:latest Rscript /root/tests/test-FRMatch.R
